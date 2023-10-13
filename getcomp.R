10fbf6c2bb84d9d450a6c6eb7800029e85208b50
Anthony Cirrincione <37804499+acirrincio@users.noreply.github.com>
2023-10-13 14:26:26 -0500

    Create getcomp.R

---
+++ getcomp.R
@@ -0,0 +1,27 @@
+#Function to get complementary DNA sequence from string 
+
+
+getcomp <- function(x) {
+    new <- ''
+    for (i in 1:nchar(x)) {
+      nuc <- substr(x,i,i)
+      new <- str_c(new,ifelse(nuc=='A','T',
+                              ifelse(nuc=='T','A',
+                                     ifelse(nuc=='G','C',
+                                            ifelse(nuc=='C','G','x')))))
+      #print(new)
+    }
+      new
+      return(new)
+  }
+
+
+
+
+# Example
+
+seq <- 'ATGGCACAGCTTT'
+
+getcomp(seq)
+
+
